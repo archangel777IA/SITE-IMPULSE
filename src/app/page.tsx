19e9a3c68b74640e6694f7717ff3024d0c7fa1f4
Arch'Angel aka icaro archanjo <archangelll333@gmail.com>
2026-01-21 15:22:21 -0300

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import LandingSections from "@/components/LandingSections";
 
-export default function HomePage() {
-  return (
-    <div className="min-h-screen">
-      <Header />
-      <main>
-        <LandingSections />
-      </main>
-      <Footer />
-    </div>
-  );
+export default function Home() {
+  return <LandingSections />;
 }
